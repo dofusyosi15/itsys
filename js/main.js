4c0f8aca656ad9ad716b5da7b0fa984be81c9ecc
Ricardo Tello Díaz <ricardo.tello@Mac-Pro-de-Administrador.local>
2017-12-19 14:19:46 -0600

    Menu
    Se agrego los 4 menus

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome to IT Systems</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>
+$(document).ready(function()
+{
+
+	// Setup variables
+	$body = $('body');
+
+    //FadeIn all sections
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+      //Cargado
+			 document.body.className = 'loaded';
+		}, 800);
+	});
+
+});
+
+function removeActive() {
+	$('.active').removeClass();
+}
+
+function homeView() {
+	removeActive();
+	$('#dvHome')[0].className = 'active';
+}
+function nosotrosView() {
+	removeActive();
+	$('#dvNosotros')[0].className = 'active';
+}
+
+function catalogoView() {
+	removeActive();
+	$('#dvCatalogo')[0].className = 'active';
+}
+
+function contactoView() {
+	removeActive();
+	$('#dvContacto')[0].className = 'active';
+}
